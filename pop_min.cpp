e8fc77d2fad35d6230bbb653d004d46782d43330
Stephan van Ellewee <stephan.van.ellewee@gmail.com>
2013-12-01 06:49:43 +0200

    removed comments, again

--- pop_min.cpp
+++ pop_min.cpp
@@ -6,158 +6,140 @@
 #include <OIS/OIS.h>
  
 using namespace Ogre; 
- 
+
 class SimpleFrameListener : public FrameListener 
 { 
 public: 
-  SimpleFrameListener(OIS::Keyboard* keyboard, OIS::Mouse* mouse) 
-  { 
-    mKeyboard = keyboard; 
-    mMouse = mouse; 
-  } 
-  // This gets called before the next frame is beeing rendered.
-  bool frameStarted(const FrameEvent& evt) 
-  {
-    //update the input devices
-    mKeyboard->capture();
-    mMouse->capture();
+    SimpleFrameListener(OIS::Keyboard* keyboard, OIS::Mouse* mouse) 
+    { 
+        mKeyboard = keyboard; 
+        mMouse = mouse; 
+    } 
+    // This gets called before the next frame is beeing rendered.
+    bool frameStarted(const FrameEvent& evt) 
+    {
+        //update the input devices
+        mKeyboard->capture();
+        mMouse->capture();
  
-    //exit if key KC_ESCAPE pressed 
-    if(mKeyboard->isKeyDown(OIS::KC_ESCAPE)) 
-      return false; 
+        //exit if key KC_ESCAPE pressed 
+        if(mKeyboard->isKeyDown(OIS::KC_ESCAPE)) 
+            return false; 
  
-    return true; 
-  } 
-  // This gets called at the end of a frame.
-  bool frameEnded(const FrameEvent& evt) 
-  { 
-    return true; 
-  } 
+        return true; 
+    } 
+    // This gets called at the end of a frame.
+    bool frameEnded(const FrameEvent& evt) 
+    { 
+        return true; 
+    } 
 private: 
-  OIS::Keyboard* mKeyboard; 
-  OIS::Mouse* mMouse; 
+    OIS::Keyboard* mKeyboard; 
+    OIS::Mouse* mMouse; 
 }; 
  
 class SimpleKeyListener : public OIS::KeyListener 
 { 
 public: 
-  bool keyPressed(const OIS::KeyEvent& e){ return true; }
+    bool keyPressed(const OIS::KeyEvent& e){ return true; }
  
-  bool keyReleased(const OIS::KeyEvent& e){ return true; }
+    bool keyReleased(const OIS::KeyEvent& e){ return true; }
 };
  
 class SimpleMouseListener : public OIS::MouseListener
 {
 public: 
-  bool mouseMoved(const OIS::MouseEvent& e){ return true; }
+    bool mouseMoved(const OIS::MouseEvent& e){ return true; }
  
-  bool mousePressed(const OIS::MouseEvent& e, OIS::MouseButtonID id){ return true; }
+    bool mousePressed(const OIS::MouseEvent& e, OIS::MouseButtonID id){ return true; }
  
-  bool mouseReleased(const OIS::MouseEvent& e, OIS::MouseButtonID id){ return true; }
+    bool mouseReleased(const OIS::MouseEvent& e, OIS::MouseButtonID id){ return true; }
 };
  
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-#define WIN32_LEAN_AND_MEAN
-#include "windows.h"
-INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+    #define WIN32_LEAN_AND_MEAN
+    #include "windows.h"
+    INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
 #else
-  int main(int argc, char **argv)
+    int main(int argc, char **argv)
 #endif
 {
-    
-  Root* root = new Root;
-
-  // Config setup 
-  ConfigFile cf;
-  cf.load("resources.cfg");
-
-  ConfigFile::SectionIterator seci = cf.getSectionIterator();
+    Root* root = new Root;
  
-  String secName, typeName, archName;
-  while (seci.hasMoreElements())
+ 
+    ConfigFile cf;
+    cf.load("resources.cfg");
+ 
+    ConfigFile::SectionIterator seci = cf.getSectionIterator();
+ 
+    String secName, typeName, archName;
+    while (seci.hasMoreElements())
     {
-      secName = seci.peekNextKey();
-      ConfigFile::SettingsMultiMap *settings = seci.getNext();
-      ConfigFile::SettingsMultiMap::iterator i;
-      for (i = settings->begin(); i != settings->end(); ++i)
+        secName = seci.peekNextKey();
+        ConfigFile::SettingsMultiMap *settings = seci.getNext();
+        ConfigFile::SettingsMultiMap::iterator i;
+        for (i = settings->begin(); i != settings->end(); ++i)
         {
-          typeName = i->first;
-          archName = i->second;
-          ResourceGroupManager::getSingleton().addResourceLocation(
-                                                                   archName, typeName, secName);
+            typeName = i->first;
+            archName = i->second;
+            ResourceGroupManager::getSingleton().addResourceLocation(
+                archName, typeName, secName);
         }
     }
-    
-  if(!root->showConfigDialog())
+
+    if(!root->showConfigDialog())
     {
-      //Ogre
-      delete root;
-      return false; // Exit the application on cancel
+        //Ogre
+        delete root;
+        return false; // Exit the application on cancel
     }
  
-  RenderWindow* window = root->initialise(true, "Simple Ogre App");
+    RenderWindow* window = root->initialise(true, "Simple Ogre App");
  
-  ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+    ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
  
-  SceneManager* mSceneMgr = root->createSceneManager(ST_GENERIC); 
+    SceneManager* sceneMgr = root->createSceneManager(ST_GENERIC); 
+    Camera* camera = sceneMgr->createCamera("SimpleCamera"); 
  
-  Camera* camera = mSceneMgr->createCamera("SimpleCamera"); 
+    Viewport* viewPort = window->addViewport(camera);
  
-  Viewport* viewPort = window->addViewport(camera);
+    OIS::ParamList pl;
+    size_t windowHnd = 0;
+    std::ostringstream windowHndStr;
  
+    window->getCustomAttribute("WINDOW", &windowHnd);
+    windowHndStr << windowHnd;
+    pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+ 
+    OIS::InputManager* inputManager = OIS::InputManager::createInputSystem(pl);
+    OIS::Keyboard* keyboard = static_cast<OIS::Keyboard*>(inputManager->createInputObject(OIS::OISKeyboard, true));
+    OIS::Mouse*    mouse = static_cast<OIS::Mouse*>(inputManager->createInputObject(OIS::OISMouse, true));
+ 
+    unsigned int width, height, depth;
+    int top, left;
+    window->getMetrics(width, height, depth, left, top);
+    const OIS::MouseState &ms = mouse->getMouseState();
+    ms.width = width;
+    ms.height = height;
+ 
+    SimpleKeyListener* keyListener = new SimpleKeyListener();
+    keyboard->setEventCallback(keyListener);
+    SimpleMouseListener* mouseListener = new SimpleMouseListener();
+    mouse->setEventCallback(mouseListener);
+    SimpleFrameListener* frameListener = new SimpleFrameListener(keyboard, mouse);
+    root->addFrameListener(frameListener); 
+ 
+    root->startRendering(); // blocks until a frame listener returns false. eg from pressing escape in this example
+ 
+    inputManager->destroyInputObject(mouse); mouse = 0;
+    inputManager->destroyInputObject(keyboard); keyboard = 0;
+    OIS::InputManager::destroyInputSystem(inputManager); inputManager = 0;
 
-
-
-  //---------------------------------------------------- 
-  // 7 add OIS input handling 
-  //----------------------------------------------------
-  OIS::ParamList pl;
-  size_t windowHnd = 0;
-  std::ostringstream windowHndStr;
+    delete frameListener; 
+    delete mouseListener; 
+    delete keyListener;
+    //Ogre
+    delete root;
  
-  //tell OIS about the Ogre window
-  window->getCustomAttribute("WINDOW", &windowHnd);
-  windowHndStr << windowHnd;
-  pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
- 
-  OIS::InputManager* inputManager = OIS::InputManager:createInputSystem(pl);
-  OIS::Keyboard* keyboard = static_cast<OIS::Keyboard*>(inputManager->createInputObject(OIS::OISKeyboard, true));
-  OIS::Mouse*    mouse = static_cast<OIS::Mouse*>(inputManager->createInputObject(OIS::OISMouse, true));
- 
-  unsigned int width, height, depth;
-  int top, left;
-  window->getMetrics(width, height, depth, left, top);
-  const OIS::MouseState &ms = mouse->getMouseState();
-  ms.width = width;
-  ms.height = height;
-
-
-  // everything is set up, now we listen for input and frames (replaces while loops)
-  //key events
-  SimpleKeyListener* keyListener = new SimpleKeyListener();
-  keyboard->setEventCallback(keyListener);
-
-  SimpleMouseListener* mouseListener = new SimpleMouseListener();
-  mouse->setEventCallback(mouseListener);
-
-  SimpleFrameListener* frameListener = new SimpleFrameListener(keyboard, mouse);
-  root->addFrameListener(frameListener); 
- 
-  root->startRendering(); // blocks until a frame listener returns false. eg from pressing escape in this example
- 
-  //----------------------------------------------------
-  // 9 clean 
-  //----------------------------------------------------
-  //OIS
-  inputManager->destroyInputObject(mouse); mouse = 0;
-  inputManager->destroyInputObject(keyboard); keyboard = 0;
-  OIS::InputManager::destroyInputSystem(inputManager); inputManager = 0;
-  //listeners
-  delete frameListener; 
-  delete mouseListener; 
-  delete keyListener;
-  //Ogre
-  delete root;
- 
-  return 0; 
+    return 0; 
 }
